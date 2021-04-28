254108c65717fe43a1772f08e950873a8a3e0b11
Adam Schoenberg <ajschoenb@utexas.edu>
2021-04-28 14:45:06 -0500

    fix OOB bug with RequestLog

--- server/src/server.rs
+++ server/src/server.rs
@@ -302,7 +302,8 @@ impl<C> Server<C> where C: RaftComms {
 
     pub fn handle_recv_request_log(&self, id: String, start_idx: usize) {
         // basically just send back as many entries as we can starting at start_idx
-        let end_idx = start_idx + 9;
+        let start_idx = min(start_idx, self.log.len());
+        let end_idx = min(start_idx + 9, self.log.len());
         let entries = (&self.log.get_vec()[start_idx..end_idx]).to_vec();
         let response = make_request_log_response(end_idx, self.log.len(), entries);
         self.comms.send(id, response);
